f548d0330f25bc4af09c5bef762ec363000791c4
vitalogy <vitalogy_github@milaw.biz>
2019-08-29 16:34:10 +0200

    add RTL8822BS

---
+++ packages/linux-drivers/RTL8822BS/package.mk
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8822BS"
+PKG_VERSION="38e702c9049f10f1cfa1f9a50d12fd21e08e306c"
+PKG_SHA256="9f966d67e332bf1fc20c1bde6c2e8083aa62904805615b361ec6bbfe44b42709"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL="https://github.com/ChalesYu/rtl8822bs-aml/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="Realtek RTL8822BS Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       CONFIG_POWER_SAVING=n \
+       USER_EXTRA_CFLAGS="-Wno-error=date-time"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+}
